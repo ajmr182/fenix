e022c63298e7a66c5b5d89eb6a1053462664ef79
Amilcar Malaver <ajmr182@gmail.com>
2025-05-14 18:21:19 -0500

    chore: Setup project

---
+++ app/src/main/java/com/eve/fenix/ui/LoginFragment.kt
@@ -0,0 +1,62 @@
+package com.eve.fenix.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.eve.fenix.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setup()
+    }
+
+    private fun setup() {
+        auth = FirebaseAuth.getInstance()
+
+        val etEmail = binding.etEmail.text
+        val etPassword = binding.etPassword.text
+        val btnLogin = binding.btnLogin
+
+        btnLogin.setOnClickListener {
+            val email = etEmail.toString()
+            val password = etPassword.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            requireContext(),
+                            "Inicio de sesión exitoso",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            "Error: ${task.exception?.message}",
+                            Toast.LENGTH_LONG
+                        )
+                            .show()
+                    }
+                }
+        }
+    }
+}
