6dd1f4c4126af771ab8306ebb07682cb491c5af4
i202311813 <TuCorreo@Cibertec.edu.pecom>
2025-05-14 19:57:37 -0500

    feat: Add navigation

---
+++ app/src/main/java/com/eve/fenix/ui/TaskFragment.kt
@@ -0,0 +1,24 @@
+package com.eve.fenix.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.eve.fenix.R
+import com.eve.fenix.databinding.FragmentLoginBinding
+import com.eve.fenix.databinding.FragmentTaskBinding
+
+class TaskFragment : Fragment() {
+
+    private var _binding: FragmentTaskBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTaskBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
